0a663922c14bcad0196a91325c9177e9c6a9b5e2
LenzOttenberger <Kirill.Drozd15@gmail.com>
2025-09-28 14:24:36 +0300

    Добавил возможность вводить слово в любом регистре в задании 2. В задании 11 сделал проверку на корректность значений. В задании 6 изменил имена переменных и перепроверил формулу.

--- task_6.py
+++ task_6.py
@@ -1,5 +1,6 @@
-p = float(input('Enter pressure (Pa): '))
-v = float(input('Enter volume (cbm): '))
-t = float(input('Enter temperature (k): '))
-n = p*v/8.31*t
-print(f'n = {round(n, 1)}')
+R = 8.31
+pressure = float(input('Enter pressure (Pa): '))
+volume = float(input('Enter volume (cbm): '))
+temperature = float(input('Enter temperature (k): '))
+p = (pressure*volume)/(R*temperature)
+print(f'p = {round(p, 1)}')
